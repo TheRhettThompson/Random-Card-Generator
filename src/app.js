7ca98b844d7c5be22c53e44ccadc5cb597cbb2eb
TheRhettThompson <rthompsoncareer@gmail.com>
2022-11-01 21:04:26 +0000

    completed project

--- src/app.js
+++ src/app.js
@@ -21,22 +21,35 @@ window.onload = function() {
     "Q"
   ];
   //create array for suits same as with numbers
-  const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  const suits = ["♠", "♣", "♥", "♦"];
 
   let newNumber = document.querySelector(".number");
   //create new variable for top-suit
-
+  let topSuit = document.querySelector(".top-suit");
   //create new variable for bottom-suit
+  let bottomSuit = document.querySelector(".bottom-suit");
 
   function randomCard(arr) {
     return Math.floor(Math.random() * arr.length);
   }
 
+  function iconCard(suits) {
+    return Math.floor(Math.random() * suits.length);
+  }
+
   function card() {
     let number = numbers[randomCard(numbers)];
-    //create variable that uses the random function to get a new suit (similar to line 15)
+    let icons = suits[iconCard(suits)];
+
+    if (icons == suits[2] || icons == suits[3]) {
+      document.getElementsByClassName("top-suit")[0].style.color = "red";
+      document.getElementsByClassName("bottom-suit")[0].style.color = "red";
+    } //create variable that uses the random function to get a new suit (similar to line 15)
 
     newNumber.innerHTML = number;
+    topSuit.innerHTML = icons;
+    bottomSuit.innerHTML = icons;
+
     //assign the new suit to the top and bottom variables using innerHTML (lines 12 and 13)
   }
 
